bf69ce67985c28749f6c17f1bf47f6eae671ef7f
suraj <surajthakurrs45@gmail.com>
2021-01-31 16:50:00 -0500

    1.0

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+        //hide the preloader
+        document.querySelector(".preloader").style.display = "none";
+    }
+
+    function clicked(){
+        alert("contact school for courses details");
+    }
